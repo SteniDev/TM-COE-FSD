ac27445d0aee7ed06cff394dc803f24bcf3d8466
STENI DEV <stenidev2003@gmail.com>
2025-02-01 21:31:09 +0530

    First Commit

---
+++ src/OOPS/Print.java
@@ -0,0 +1,7 @@
+package OOPS;
+
+public class Print {
+    public static final String ENGINE_STARTED = " ENGINE  IS STARTED";
+    public static final String ELECTRIC_ENGINE_STARTED = " ENGINE OF  ELECTRIC CAR IS STARTED";
+    public static final String BATTERY_CHARGED = "THE BATTERY IS BEING CHARGED";
+}
