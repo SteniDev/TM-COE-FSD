3d9b05d30dcf7114146b95d3a1d8272a9f3e8bf2
STENI DEV <stenidev2003@gmail.com>
2025-02-01 22:06:16 +0530

    First Commit

--- src/OOPS/Print.java
+++ src/OOPS/Print.java
@@ -4,4 +4,6 @@ public class Print {
     public static final String ENGINE_STARTED = " ENGINE  IS STARTED";
     public static final String ELECTRIC_ENGINE_STARTED = " ENGINE OF  ELECTRIC CAR IS STARTED";
     public static final String BATTERY_CHARGED = "THE BATTERY IS BEING CHARGED";
+    public static final String QUOTIENT = "Quotient of the division is ";
+    public static final String REMAINDER = "Remainder of the division is ";
 }
