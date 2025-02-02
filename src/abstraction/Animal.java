79d3c4bb0ef5e3d41d47e7740cad4ecc35600580
STENI DEV <stenidev2003@gmail.com>
2025-02-02 18:46:06 +0530

    First Commit

---
+++ src/abstraction/Animal.java
@@ -0,0 +1,7 @@
+package abstraction;
+
+abstract class Animal {
+    //Abstract method to be implemented in subclasses
+    abstract void makeSound();
+}
+
---
+++ src/abstraction/TestAnimal.java
@@ -0,0 +1,11 @@
+package abstraction;
+
+public class TestAnimal {
+    public static void main(String[] args) {
+        // Creating an instance of Dog
+        Dog dog = new Dog();
+
+        // Calling the makeSound method
+        dog.makeSound();
+    }
+}
